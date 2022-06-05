b1afce0ce8abf52833019b7321784c0a88d485a1
Kerolyne Dias <ribeirodias2003@gmail.com>
2022-06-05 14:26:00 -0300

    classe Banco configurada

--- Banco.cs
+++ Banco.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,34 @@ namespace nutrisystem1._2
 {
     internal class Banco
     {
+        private String host = "localhost";
+        private String nomeBanco = "nutrisystem";
+        private String usuario = "root";
+        private String senha = "dias004756*";
+        private MySqlConnection con;
+        private MySqlCommand cmd;
+
+        public Banco()
+        {
+            Conectar();
+        }
+
+        public void Conectar()
+        {
+            String stringCon = "SERVER=" + this.host + ";DATABASE=" + this.nomeBanco + ";UID=" + this.usuario + ";PASSWORD=" + this.senha + ";";
+            this.con = new MySqlConnection(stringCon);
+            this.cmd = this.con.CreateCommand();
+            this.con.Open();
+        }
+        public void NonQuerry(String sql)
+        {
+            this.cmd.CommandText = sql;
+            this.cmd.ExecuteNonQuery();
+        }
+        public MySqlDataReader Querry(String sql)
+        {
+            this.cmd.CommandText = sql;
+            return this.cmd.ExecuteReader();
+        }
     }
 }
